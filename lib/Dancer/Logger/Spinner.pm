e288b703bfa594b33384cf12117216c9bab5ccd5
Sawyer X <xsawyerx@cpan.org>
2010-11-01 10:52:35 +0200

    adding documentation

--- lib/Dancer/Logger/Spinner.pm
+++ lib/Dancer/Logger/Spinner.pm
@@ -40,3 +40,26 @@ sub DESTROY {
 
 __END__
 
+=head1 SYNOPSIS
+
+    # in your Dancer app:
+    setting logger => 'spinner';
+
+    # or in your Dancer config file:
+    logger: 'spinner'
+
+Et voila!
+
+=head1 DESCRIPTION
+
+When using this logger and running your application in the terminal, you will
+see a text spinner running on each message it gets. If you have a page with a
+lot of request, and they will come in fast, you'll see the spinner running. If
+you have an app with very little requests in each page or if it is slow, the
+spinner will run slowly.
+
+Each request matches another rotation of the spinner.
+
+=head1 SEE ALSO
+
+The Dancer Advent Calendar 2010.
